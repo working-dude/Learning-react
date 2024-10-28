57a12b2566d269a1a97473c49cbe702c355ccaa9
working-dude <120950957+working-dude@users.noreply.github.com>
2024-10-28 21:28:51 +0530

    passing props and no function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // import Kanishk from "./first";
 import Listgroup from "./components/Listgroup";
+var name = ["Kanishk", "Aman", "Rahul", "Rohit", "Raj", "1"];
+
 function App() {
-  return <Listgroup></Listgroup>;
+  return <Listgroup name={name} heading="hero"></Listgroup>;
 }
 export default App;
--- src/components/Listgroup.tsx
+++ src/components/Listgroup.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 
-function Listgroup() {
-  var name = ["Kanishk", "Aman", "Rahul", "Rohit", "Raj"];
+// Passing props to the component
+interface ListgroupProps {
+  name: string[];
+  heading: string;
+}
+
+function Listgroup({ name, heading }: ListgroupProps) {
+  // var name = ["Kanishk", "Aman", "Rahul", "Rohit", "Raj"];
   // name = [];
-  // Hook to store the state of the component. It returns an array with two elements. The first element is the current value of the state, and the second element is a function that lets you update it.
+  // Hook to store the state of the component. It returns an array with two elements.
+  //  The first element is the current value of the state, and the second element is a function that lets you update it.
   const [i, selected_index] = useState(0);
   function selected(index: number, index_selected: number) {
     if (index === index_selected) {
@@ -15,7 +22,7 @@ function Listgroup() {
   return (
     // Fragment is used to return multiple elements without adding extra nodes to the DOM
     <>
-      <h1>List of random names</h1>
+      <h1>{heading} of random names</h1>
       <ul className="list-group">
         {name.length === 0 && <h1>No names to display</h1>}
         {/* Travel the name list using map function and give key for each item to perform crud on it */}
