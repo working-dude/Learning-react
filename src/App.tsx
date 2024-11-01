46cad8e701dc596677fe327006c65f96eea7ede0
working-dude <120950957+working-dude@users.noreply.github.com>
2024-11-01 16:56:47 +0530

    button color change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 // import Kanishk from "./first";
 import Listgroup from "./components/Listgroup";
 import Hello from "./components/Hello";
+import { useState } from "react";
+// import { useEffect} from "react";
 
 function App() {
-  var name = ["Kanishk", "Aman", "Rahul", "Rohit", "Raj", "1"];
+  var name = ["Kanishk", "Aman", "Rahul", "Rohit", "Raj"];
+  const [i, button] = useState(0);
   var selecteditem = (name: string) => {
     console.log(name);
   };
+
+  // Function to change the color of the button continuously
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     button((prev) => (prev + 1) % 7);
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+  function buttons(i: Number) {
+    switch (i) {
+      case 1:
+        return "btn btn-primary";
+      case 2:
+        return "btn btn-secondary";
+      case 3:
+        return "btn btn-success";
+      case 4:
+        return "btn btn-danger";
+      case 5:
+        return "btn btn-warning";
+      case 6:
+        return "btn btn-info";
+
+      default:
+        return "btn btn-light";
+    }
+  }
 
   return (
     <>
@@ -18,10 +49,15 @@ function App() {
       ></Listgroup>
       <Hello>
         <h1>hello</h1>
-        <button className="btn btn-primary">Primary</button>
-        <button className="btn btn-secondary">secondary</button>
-        <button className="btn btn-warning">warning</button>
-        <button className="btn btn-dark">dark</button>
+        <button
+          className={buttons(i)}
+          onClick={() => {
+            // useEffect;
+            button((prev) => (prev + 1) % 7);
+          }}
+        >
+          Submit
+        </button>
       </Hello>
     </>
   );
